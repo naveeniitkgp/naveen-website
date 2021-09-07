7a479f23f373054ab6caadfe9377b618248b6e63
Naveen  Kumar <mnaveen.iitkgp@gmail.com>
2021-09-07 17:58:21 +0530

    first commit with about page and whoa we are in biz

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>Hi! I'm Naveen Kumar Maddu. This site is deticated to my research in Computer-aided design 
+         and Machine Learning.  
+      </p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
